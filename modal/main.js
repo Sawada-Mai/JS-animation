2c30e407aaf37075bfd3168f75e03b6232c201b0
msawada <sawadamai@sawadamainoMac-mini.local>
2024-01-04 00:31:03 +0900

    add move_line

--- modal/main.js
+++ modal/main.js
@@ -1,13 +1,8 @@
-$(document).ready(function() {
-  // モーダルを開くためのリンクをクリックしたときの処理
-  $('[data-modal="modal"]').on('click', function(e) {
-    e.preventDefault();
-    $('.modal').addClass('active');
-  });
-
-  // モーダルを閉じるためのボタンをクリックしたときの処理
-  $('[data-modal="close"]').on('click', function(e) {
-    e.preventDefault();
-    $('.modal').removeClass('active');
+$(function () {
+  // フェードイン
+  $(".blockHidden").on("inview", function (event, isInView) {
+      if (isInView) {
+          $(this).addClass("showBlock");
+      }
   });
 });
---
+++ move_line/main.js
@@ -0,0 +1,8 @@
+$(function () {
+    // フェードイン
+    $(".blockHidden").on("inview", function (event, isInView) {
+        if (isInView) {
+            $(this).addClass("showBlock");
+        }
+    });
+  });
